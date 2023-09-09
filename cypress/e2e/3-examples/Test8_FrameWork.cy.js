cb7fc57534b0b59ad3ab9cef000a99baabd424fb
Srijon Biswas <60281395+DSrijon01@users.noreply.github.com>
2023-09-10 01:05:49 +0600

    Test 8 fixture, command update

--- cypress/e2e/3-examples/Test8_FrameWork.cy.js
+++ cypress/e2e/3-examples/Test8_FrameWork.cy.js
@@ -1,4 +1,4 @@
-// Data driven testing with fixture. 
+// Data driven testing with fixture custom commands support.
 /// <reference types="Cypress" />
 
 describe('My eighth Test Suite', function() {
@@ -30,17 +30,39 @@ describe('My eighth Test Suite', function() {
         cy.get('#inlineRadio1').should('be.enabled')
         cy.get('#inlineRadio2').should('be.enabled')
         cy.get('#inlineRadio3').should('be.disabled')
-
+        // cy.pause()
         //navigate to the shop menu button 
         cy.get(':nth-child(2) > .nav-link').click()
+        
+        // Selecting a product form the shop page - Hard Coded 
+        cy.get('h4.card-title').each(($el, index, $list) => {
+            if($el.text().includes('Blackberry')){
 
-        // cy.pause()
-
+                cy.get('button.btn.btn-info').eq(index).click()
+        }
+            
+        })
+        // Selecting a product form the shop page from Support --> Custom Commands
+        cy.selectProduct('Nokia Edge')
+        // Selecting a product form the shop page from Fixture--> Support --> Custom Commands
         this.data.productName.forEach(function(element) {
 
             cy.selectProduct(element)
 
         });
+
+        // Assertion that all 3 products got slected
+        cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').click()
+        const expectedProducts = ['Blackberry', 'Nokia Edge', 'iphone X'];
+        // Iterate through the expected product names and assert them - With a Loop
+        expectedProducts.forEach((product) => {
+        cy.get(':nth-child(1) > .col-sm-8, :nth-child(2) > .col-sm-8, :nth-child(3) > .col-sm-8 ').should('contain.text', product);
+        });
+
+        // Assert that all three products are selected - Hard Coded
+        cy.get(':nth-child(1) > .col-sm-8').should('contain.text', 'Blackberry');
+        cy.get(':nth-child(2) > .col-sm-8').should('contain.text', 'Nokia Edge');
+        cy.get(':nth-child(3) > .col-sm-8').should('contain.text', 'iphone X');
 
 
     })
