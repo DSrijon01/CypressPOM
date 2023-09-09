54de9a4a212bf077b96a3a207021a6f3c1e41121
Srijon Biswas <60281395+DSrijon01@users.noreply.github.com>
2023-09-09 23:22:52 +0600

    Templates Streamlining

---
+++ cypress/e2e/3-examples/Test_09_new.cy.js
@@ -0,0 +1,32 @@
+// Test with Cypress Page Object Model
+/// <reference types="Cypress" />
+
+describe('Page Object Model Learning', function()
+{
+    before(function()
+    {
+        
+
+    })
+
+
+
+
+
+    it('My First Test with Page Object Model', function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+})
