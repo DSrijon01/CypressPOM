7b1731022f206dc4dea8a25d5bb5733591103480
Srijon Biswas <60281395+DSrijon01@users.noreply.github.com>
2023-09-18 15:22:28 +0600

    API Test HTTP Response Mocking

---
+++ cypress/e2e/3-examples/API_Test.cy.js
@@ -0,0 +1,46 @@
+// Api Test Navigation with
+ 
+describe('My 10th Test Suite', function() 
+{
+ 
+it('Api Response Assertion',function() {
+
+    cy.visit("https://rahulshettyacademy.com/angularAppdemo/")
+    cy.wait(2000)
+    
+ // Mocking API response
+cy.intercept({
+
+    method : 'Get',
+    url : 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+
+
+},
+{
+    statusCode : 200,
+    body : [
+        {
+            "book_name": "RestAssured with Java",
+            "isbn": "BSG",
+            "aisle": "2302"
+        }
+    ]
+    
+
+
+}).as('bookretrievals')
+cy.get('.btn-primary').click()
+cy.wait('@bookretrievals') 
+cy.get('p').should('have.text', "Oops only 1 Book available")
+
+// 
+
+
+
+
+
+
+})
+ 
+ 
+})
